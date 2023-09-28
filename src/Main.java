d9b37e6e39b5a5d68d7e58c0899eb6f0b1fe4e1f
dashoes15 <daltonasali@gmail.com>
2023-09-28 09:09:52 -0400

    lab 5 task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int age = 0;
+        System.out.println("Please enter your age:");
+        if (scan.hasNextInt()) {
+            age = scan.nextInt();
+        }else{
+            System.out.println("You have entered an invalid value.");
+            System.exit(0);
+        }
+        if (age >= 21) {
+            System.out.println("You get a wristband!");
+        }else{
+            System.exit(0);
+        }
     }
 }
